869c35d868380fcd03db35c1414909e389835c3d
Adebayo Samuel <eladebayoor@gmail.com>
2022-08-05 23:21:41 +0100

    [fix]: user authentification features added

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,29 @@
 import { getInitialData } from '../utils/api'
 import { recieveUsers } from '../actions/users'
 import { recieveQuestions } from '../actions/questions'
-import { setAuthedUser } from '../actions/authedUser'
-
-const AUTHED_ID = 'tylermcginnis'
+import { setAuthedUser, unsetAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(recieveUsers(users))
         dispatch(recieveQuestions(questions))
-        dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading())
       })
+  }
+}
+
+export function signIn (AUTHED_ID) {
+  return (dispatch) => {
+    dispatch(setAuthedUser(AUTHED_ID))
+  }
+}
+
+export function signOut () {
+  return (dispatch) => {
+    dispatch(unsetAuthedUser())
   }
 }
